b76659a3dc0b031e7dda6a983ef69804e85a2aa7
Tehillahk <kangambt@myumanitoba.ca>
2024-10-29 01:01:08 -0500

    kinda works but loads twice for some reason

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import ePub from "epubjs";
 
+import {useEffect, useRef, useState} from "react";
 
 function App() {
+  const epubRef = useRef<HTMLDivElement|null>(null);
 
+
+
+  useEffect(() => {
+    const book = ePub("https://zfzqwolxrejrzidhmsai.supabase.co/storage/v1/object/public/pages/books/GROOTSLANG_BEMBA.epub")
+    const rendition = book.renderTo(epubRef.current as HTMLDivElement, {width: 600, height: 900});
+    rendition.display();
+  },[])
 
   return (
-    <div>
+    <div ref={epubRef}>
+
 
     </div>
   )
